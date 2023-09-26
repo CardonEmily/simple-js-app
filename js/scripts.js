85433015e2d752366534626331c340713b4fb442
CardonEmily <141743610+CardonEmily@users.noreply.github.com>
2023-09-26 12:24:28 -0400

    Compile First Few Pokemon
    Cleared the JS file and proceeded to started adding in pokemon using specified variable and array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-alert('Hello World!');
-let favoriteFood = 'Dutch Oven Potatoes'
-document.write(favoriteFood)
+let pokemonList = [
+
+    {name: 'Ninetales',
+    height: 1.1,
+    types: ['Fire'],
+    abilities: ['Flash-Fire', 'Drought']
+    }
+
+    {name: 'Dewgong',
+    height: 1.7,
+    type: ['Ice', 'Water'],
+    abilities: ['Thick-Fat', 'Hyrdration', 'Ice-Body']
+    }
+]
