2a173d6d7e4d7e8cacf73b830184cee2fd361abd
CardonEmily <141743610+CardonEmily@users.noreply.github.com>
2023-09-26 12:35:58 -0400

    Adding addition Pokemon
    Organizing the final Pokemon in the js file.

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,29 @@ let pokemonList = [
     height: 1.1,
     types: ['Fire'],
     abilities: ['Flash-Fire', 'Drought']
-    }
+    },
 
     {name: 'Dewgong',
     height: 1.7,
     type: ['Ice', 'Water'],
     abilities: ['Thick-Fat', 'Hyrdration', 'Ice-Body']
-    }
+    },
+
+    {name: 'Tropius',
+    height: '2',
+    type: ['Grass', 'Flying'],
+    abilities: ['Chlorophyll', 'Solarpower', 'Harvest']
+    },
+
+    {name: 'Reshiram',
+    height: 3.2,
+    type: ['Fire', 'Dragon'],
+    abilities: ['Turboblaze']
+    },
+
+    {name: 'Gardevoir',
+    height: 1.6,
+    type: ['Fairy', 'Psychic'],
+    abilities: ['Syncronize', 'Trace', 'Telepathy']
+}
 ]
