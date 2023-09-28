8156c2ab27503e7f107761bad99a6b984dc63010
CardonEmily <141743610+CardonEmily@users.noreply.github.com>
2023-09-28 10:31:57 -0400

    Fixed spelling errors
    Installed and utilized spell check plug-in. Edited the errors previously made.

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,13 @@ let pokemonList = [
     {name: 'Dewgong',
     height: 1.7,
     type: ['Ice', 'Water'],
-    abilities: ['Thick-Fat', 'Hyrdration', 'Ice-Body']
+    abilities: ['Thick-Fat', 'Hydration', 'Ice-Body']
     },
 
     {name: 'Tropius',
     height: '2',
     type: ['Grass', 'Flying'],
-    abilities: ['Chlorophyll', 'Solarpower', 'Harvest']
+    abilities: ['Chlorophyll', 'Solar Power', 'Harvest']
     },
 
     {name: 'Reshiram',
@@ -27,12 +27,12 @@ let pokemonList = [
     {name: 'Gardevoir',
     height: 1.6,
     type: ['Fairy', 'Psychic'],
-    abilities: ['Syncronize', 'Trace', 'Telepathy']
+    abilities: ['Synchronize', 'Trace', 'Telepathy']
 }
 ]
 
 for (let i = 0; i < pokemonList.length; i++){
-    if (pokemonList[i].height > 1.9) { //setting minimun height for Pokemon to be considered big.
+    if (pokemonList[i].height > 1.9) { //setting minimum height for Pokemon to be considered big.
      document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm) Wow, that\'s a big one!<br>');
     } else if (pokemonList[i].height < 1.9 && pokemonList[i].height > 1) { //setting average size regulations.
      document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm) That seems to be an average Pokemon.<br>');
