902485bb98fb8898e5e52e48c406c58732b6b220
CardonEmily <141743610+CardonEmily@users.noreply.github.com>
2023-10-23 16:46:52 -0400

    Added IIFE
    Added pokemonRepository variable and assigned the IIFE. Input the getAll and Add functions.
    
    Also applied formatting.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,70 @@
-let pokemonList = [
-
-    {name: 'Ninetales',
-    height: 1.1,
-    type: ['Fire'],
-    abilities: ['Flash-Fire', 'Drought']
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Ninetales",
+      height: 1.1,
+      type: ["Fire"],
+      abilities: ["Flash-Fire", "Drought"],
     },
 
-    {name: 'Dewgong',
-    height: 1.7,
-    type: ['Ice', 'Water'],
-    abilities: ['Thick-Fat', 'Hydration', 'Ice-Body']
+    {
+      name: "Dewgong",
+      height: 1.7,
+      type: ["Ice", "Water"],
+      abilities: ["Thick-Fat", "Hydration", "Ice-Body"],
     },
 
-    {name: 'Tropius',
-    height: '2',
-    type: ['Grass', 'Flying'],
-    abilities: ['Chlorophyll', 'Solar Power', 'Harvest']
+    {
+      name: "Tropius",
+      height: 2,
+      type: ["Grass", "Flying"],
+      abilities: ["Chlorophyll", "Solar Power", "Harvest"],
     },
 
-    {name: 'Reshiram',
-    height: 3.2,
-    type: ['Fire', 'Dragon'],
-    abilities: ['Turboblaze']
+    {
+      name: "Reshiram",
+      height: 3.2,
+      type: ["Fire", "Dragon"],
+      abilities: ["Turboblaze"],
     },
 
-    {name: 'Gardevoir',
-    height: 1.6,
-    type: ['Fairy', 'Psychic'],
-    abilities: ['Synchronize', 'Trace', 'Telepathy']
-}
-]
+    {
+      name: "Gardevoir",
+      height: 1.6,
+      type: ["Fairy", "Psychic"],
+      abilities: ["Synchronize", "Trace", "Telepathy"],
+    },
+  ];
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: "Pikachu" });
+
+pokemonRepository
+  .getAll()
+  .forEach((pokemon) =>
+    document.write(
+      pokemon.name +
+        " (" +
+        pokemon.type +
+        ")," +
+        " has a height of " +
+        pokemon.height +
+        "m. <br> "
+    )
+  );
 
 // for (let i = 0; i < pokemonList.length; i++){
 //     if (pokemonList[i].height > 1.9) { //setting minimum height for Pokemon to be considered big.
@@ -38,7 +73,3 @@ let pokemonList = [
 //      document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm) That seems to be an average Pokemon.<br>');
 //     }
 // }
-// userList.forEach(function(user) {
-//   console.log(user.name + ' is ' + user.age + ' years old.');
-// });
-pokemonList.forEach( pokemon => document.write(pokemon.name + ' \(' + pokemon.type + '\),' + ' has a height of ' + pokemon.height + 'm. <br> '))
